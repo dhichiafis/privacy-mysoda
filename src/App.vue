4dda3d31deff2b45f66b177e424576553f16c5e3
dhichiafis <ochiengodhiambo56@gmail.com>
2026-06-21 08:55:19 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Policy from './components/Policy.vue';
+</script>
+
+<template>
+  <Policy/>
+</template>
---
+++ src/components/Policy.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+<template>
+    <div class="main flex h-full w-full bg-white">
+        <div class=" p-8 md:max-w-md relative flex flex-col mx-auto mt-6">
+            <h1 class=" text-gray-900 text-[28px]">Privacy Policy</h1>
+
+        <p>Last Updated: June 2026</p>
+
+        <p>This app collects phone numbers solely for processing payments and claims.</p>
+
+        <p>We do not sell or share your personal information with third parties.</p>
+
+        <p class="absolute bottom-[12px] ">Contact: support@msivu.xyz</p>
+        </div>
+    </div>
+</template>
